9aca6723eba986c90557c786284454135a5e2a32
WesleyChang <ioh04019@gmail.com>
2019-08-20 15:54:31 +0800

    Update endpoint

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3002'
+  // baseURL: 'http://localhost:3002'
+  baseURL: 'https://my-awesome-project.herokuapp.com/'
   // baseURL: 'https://my-json-server.typicode.com/WeiChiaChang/fb-post'
 })
 
